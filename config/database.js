e979e83630ba430235eccc99379789f12c90e341
Shahid Khan <57123934+Shahidkhan0786@users.noreply.github.com>
2022-10-08 23:29:27 +0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const dbconnection = () => {
-    mongoose.connect(process.env.DB_CONNECTION_URL)
+    mongoose.connect(process.env.DB_CONNECTION_URL,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
     .then(() => {
         console.log('DB connected successfully');
     }).catch(err => {
